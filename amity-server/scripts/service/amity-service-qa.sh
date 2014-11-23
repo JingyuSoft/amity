3252c6ee008f7bd63617a234d97a679d0d18ee45
Gengyu Shi <univer.shi@gmail.com>
2014-11-23 19:05:33 +0000

    script

--- amity-server/scripts/service/amity-service-qa.sh
+++ amity-server/scripts/service/amity-service-qa.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-cd `dirname $0`
+DIRNAME=`dirname $0`
+BASENAME=`basename $0`
+if [ ! -f $DIRNAME/amity-service.sh ]; then
+	DIR_NAME=`readlink $DIR_NAME/$BASENAME`
+fi
+
+cd $DIR_NAME
 ./amity-service.sh qa

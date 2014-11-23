88e7d9981841ede68b0a706c1140fde25095fd87
Gengyu Shi <univer.shi@gmail.com>
2014-11-23 20:25:58 +0000

    Minot changes to scripts

--- amity-server/build.sh
+++ amity-server/build.sh
@@ -8,3 +8,10 @@ mvn package
 
 cp scripts/service/* target/appassembler/bin
 chmod +x target/appassembler/bin/*
+
+if [ ! -L /etc/init.d/amity-service-qa ]; then
+	echo "Registering amity-service-qa as Linux service..."
+	sudo ln -s target/appassembler/bin/amity-service-qa /etc/init.d/amity-service-qa
+else
+	echo "amity-service-qa already registered as Linux service"
+fi

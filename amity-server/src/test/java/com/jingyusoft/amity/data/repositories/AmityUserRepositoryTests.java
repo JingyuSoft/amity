efb6e3b8b628744e8b7cf92eeba4a972b6975907
Gengyu Shi <univer.shi@gmail.com>
2014-11-29 20:49:22 +0000

    Create unit test database

---
+++ amity-server/src/test/java/com/jingyusoft/amity/data/repositories/AmityUserRepositoryTests.java
@@ -0,0 +1,15 @@
+package com.jingyusoft.amity.data.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/com/jingyusoft/amity/config/application-context-unit-test.xml")
+public class AmityUserRepositoryTests {
+
+	@Test
+	public void testDummy() {
+	}
+}

cb10548193ade32ce2966e9ca989ed4117d20511
Gengyu Shi <univer.shi@gmail.com>
2014-11-30 20:14:23 +0000

    -

--- amity-server/src/test/java/com/jingyusoft/amity/authentication/AuthenticationServiceTests.java
+++ amity-server/src/test/java/com/jingyusoft/amity/authentication/AuthenticationServiceTests.java
@@ -2,6 +2,7 @@ package com.jingyusoft.amity.authentication;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,8 +21,12 @@ public class AuthenticationServiceTests {
 
 	@Test
 	public void testAuthenticateFacebookAccount() {
-		AmityUser amityUser = authenticationService
-				.authenticateFacebookAccount("CAACEdEose0cBAAI6igH7emPjSOBMI1LK3zEZCOfyAZB3GzcPPBhnJ6adCx7qdyZB2LJhZAylK7zrJRkh9BfnxJMWFcYYgoOEuDqav1ayleLmtuNL2SsRY8wotIo0OyrOZCDKOyS8FaYO3sTz5EB0YQiNE5VNHYzZCzFzdA7zp9gl39wTMreoRgZBK6MDUcExpZBLjtPZBNG1mCFUYvj6fQDt2ipBqiUeNAC4ZD");
-		Assert.assertNotNull(amityUser);
+
+		final String facebookToken = System.getProperty("facebook.token");
+
+		if (!StringUtils.isEmpty(facebookToken)) {
+			AmityUser amityUser = authenticationService.authenticateFacebookAccount(facebookToken);
+			Assert.assertNotNull(amityUser);
+		}
 	}
 }

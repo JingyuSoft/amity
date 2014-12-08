4a526c451b3982cb7e330e11ff4f29985f554f74
Gengyu Shi <univer.shi@gmail.com>
2014-12-08 22:01:20 +0000

    -

--- amity-server/src/test/java/com/jingyusoft/amity/authentication/AuthenticationServiceTest.java
+++ amity-server/src/test/java/com/jingyusoft/amity/authentication/AuthenticationServiceTest.java
@@ -5,12 +5,14 @@ import javax.annotation.Resource;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.jingyusoft.amity.config.UnitTestConfigConstants;
 import com.jingyusoft.amity.domain.AmityUser;
+import com.jingyusoft.amity.testgroups.DatabaseRequired;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(UnitTestConfigConstants.APPLICATION_CONTEXT_PATH)
@@ -20,6 +22,7 @@ public class AuthenticationServiceTest {
 	private AuthenticationService authenticationService;
 
 	@Test
+	@Category(DatabaseRequired.class)
 	public void testAuthenticateFacebookAccount() {
 
 		final String facebookToken = System.getProperty("facebook.token");

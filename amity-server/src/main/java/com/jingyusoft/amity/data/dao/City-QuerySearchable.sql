e4de3d9bdf34ba8190342590b7519dc20e89ee3a
Gengyu Shi <univer.shi@gmail.com>
2014-12-19 17:22:17 +0000

    -

--- amity-server/src/main/java/com/jingyusoft/amity/data/dao/City-QuerySearchable.sql
+++ amity-server/src/main/java/com/jingyusoft/amity/data/dao/City-QuerySearchable.sql
@@ -5,4 +5,3 @@ UNION
 SELECT ci.id, ci.name as city_name, "" as region_name, co.name as country_name from city ci
 INNER JOIN country co on ci.country_id = co.id
 WHERE ci.region_id is null
-LIMIT 10000;

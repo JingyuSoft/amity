bfaae1062baeb32dfa07eb893df0386413c59d52
Gengyu Shi <univer.shi@gmail.com>
2014-12-30 20:36:54 +0000

    -

--- amity-server/src/main/java/com/jingyusoft/amity/data/repositories/HelpRequestRepository.java
+++ amity-server/src/main/java/com/jingyusoft/amity/data/repositories/HelpRequestRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.jingyusoft.amity.data.entities.HelpRequestEntity;
@@ -12,5 +13,5 @@ import com.jingyusoft.amity.data.entities.HelpRequestEntity;
 public interface HelpRequestRepository extends JpaRepository<HelpRequestEntity, Long> {
 
 	@Query("SELECT i FROM HelpRequestEntity i WHERE i.user.id = :amityUserId")
-	List<HelpRequestEntity> listHelpRequests(final long amityUserId);
+	List<HelpRequestEntity> listHelpRequests(@Param("amityUserId") final long amityUserId);
 }
